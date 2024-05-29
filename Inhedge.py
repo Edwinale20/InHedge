df408fd646768959a8b60a05258dd8cfd945e138
Edwinale20 <136374991+Edwinale20@users.noreply.github.com>
2024-05-29 00:50:42 -0600

    Update Inhedge.py

--- Inhedge.py
+++ Inhedge.py
@@ -14,7 +14,7 @@ def load_lottiefile(filepath: str):
         return json.load(f)
 
 # Cargar la animación Lottie
-lottie_animation = load_lottiefile("ora.json")
+lottie_animation = load_lottiefile("inhedge.json")
 
 # Personalización de estilos y título
 st.markdown("""
@@ -128,16 +128,6 @@ df_resultados_fx = pd.DataFrame(resultados_fx, columns=['Precio Spot FX', 'Pérd
 st.subheader("Resultados de la Cobertura de Divisas")
 st.table(df_resultados_fx)
 
-# Cargar la animación Lottie adicional
-lottie_tarjeta = load_lottiefile("tarjeta.json")
-
-# Mostrar la animación Lottie adicional en el centro de la página usando columnas
-col1, col2, col3 = st.columns([1, 2, 1])
-
-with col2:
-    st_lottie(lottie_tarjeta, key='tarjeta', height=300, width=300)
-
-
 # Gráfica de Pérdida y Ganancia Máxima de Divisas
 df_grafica_fx = df_resultados_fx[['Pérdida Máxima FX', 'Ganancia Máxima FX']].melt(var_name='variable', value_name='value')
 fig_fx = px.bar(df_grafica_fx, x=df_grafica_fx.index, y='value', color='variable', barmode='group', title="Pérdida y Ganancia Máxima de Divisas")
@@ -166,4 +156,12 @@ st.write("""
    - Además, se muestra una tabla y gráfica de la cobertura de divisas, si aplica.
 """)
 
+# Cargar la animación Lottie adicional
+lottie_tarjeta = load_lottiefile("tarjeta.json")
+
+# Mostrar la animación Lottie adicional en el centro de la página usando columnas
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:
+    st_lottie(lottie_tarjeta, key='tarjeta', height=300, width=300)
 
